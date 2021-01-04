7361b2677040b8090799a5fa7ea7362fe2161bc7
amir <olempico@gmail.com>
2021-01-04 11:36:25 +0330

    Creates CategoryFragment.

--- app/src/main/java/com/multithread/dindinntest/ui/venue/category/CategoryFragment.kt
+++ app/src/main/java/com/multithread/dindinntest/ui/venue/category/CategoryFragment.kt
@@ -8,15 +8,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.multithread.dindinntest.databinding.FragmentCategoryBinding
 import com.multithread.dindinntest.domain.entity.CategoryEntity
+import com.multithread.dindinntest.domain.entity.FoodEntity
 import dagger.android.support.AndroidSupportInjection
 
 
 class CategoryFragment : Fragment() {
 
+
     private var category: CategoryEntity = CategoryEntity()
+
     private var _binding: FragmentCategoryBinding? = null
     private val binding
         get() = _binding!!
+
+    private lateinit var foodListAdapter: FoodRecyclerViewAdapter
+
+    val callback: (FoodEntity) -> Unit = {
+
+    }
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -39,7 +48,7 @@ class CategoryFragment : Fragment() {
     }
 
     private fun setupRecycler() {
-
+        foodListAdapter = FoodRecyclerViewAdapter(category.products, callback)
     }
 
     private fun getInfo() {
